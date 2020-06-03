7d0c1e2adf7ee7ca62a0386822b9ebdad763f685
iii <cclan8@outlook.com>
2020-06-03 15:15:26 +0800

    简化一下

--- model.py
+++ model.py
@@ -3,11 +3,13 @@ from gluoncv.model_zoo import get_model
 
 ctx = mx.gpu()
 
-detector_name = "ssd_512_mobilenet1.0_coco"
-detector = get_model(detector_name, pretrained=True, ctx=ctx)
+detector = get_model(
+    "ssd_512_mobilenet1.0_coco",
+    pretrained=True, ctx=ctx)
 
-estimator_name = "simple_pose_resnet18_v1b"
-estimator = get_model(estimator_name, pretrained='ccd24037', ctx=ctx)
+estimator = get_model(
+    "simple_pose_resnet18_v1b",
+    pretrained='ccd24037', ctx=ctx)
 
 detector.reset_class(classes=['person'], reuse_weights={'person':'person'})
 
