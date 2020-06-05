f8eb85a2a59ae9105ff61a4ea3a4652838acdd3e
iii <cclan8@outlook.com>
2020-06-05 11:11:22 +0800

    向量化计算关节角度

--- angle.py
+++ angle.py
@@ -2,10 +2,9 @@ import numpy as np
 from mxnet import ndarray
 from sklearn.metrics import mean_squared_error
 
-np.seterr(invalid='ignore')
 # 需要测量角度的部位，每个部位需要用它本身和与之连接的两个节点来计算角度
 # 第一点是关节点
-KeyPoints = [
+KeyPoints = np.array([
     (5, 6, 7),      # 左肩
     (6, 5, 8),      # 右肩
     (7, 5, 9),      # 左臂
@@ -14,7 +13,7 @@ KeyPoints = [
     (12, 6, 14),    # 右胯
     (13, 11, 15),   # 左膝
     (14, 12, 16),   # 右膝
-]
+])
 
 class AngeleCal():
 
@@ -28,25 +27,22 @@ class AngeleCal():
         joint_visible = confidence[:, :, 0] > keypoint_thresh
         angles = np.empty((coords.shape[0], len(KeyPoints)))
 
-        for i, pts in enumerate(coords):
-            # 某个人
-            for j, keyPoint in enumerate(KeyPoints):
-                # 是否识别到这个关节
-                if joint_visible[i, keyPoint[0]] and joint_visible[i, keyPoint[1]] and joint_visible[i, keyPoint[2]]:
-                    # 坐标
-                    p0x = pts[keyPoint[0], 0].asscalar()
-                    p0y = pts[keyPoint[0], 1].asscalar()
-                    p1x = pts[keyPoint[1], 0].asscalar()
-                    p1y = pts[keyPoint[1], 1].asscalar()
-                    p2x = pts[keyPoint[2], 0].asscalar()
-                    p2y = pts[keyPoint[2], 1].asscalar()
+        for i, pts in enumerate(coords):                                            # 循环计算每个人
+            joint_computable = joint_visible[i, KeyPoints].asnumpy().all(axis=1)    # 可计算的关节
+            angles[i][~joint_computable] = np.nan                                   # 不可计算的关节设置nan
+            keyPoint = KeyPoints[joint_computable]                                  # 要计算的关节
+            if len(keyPoint):                                                       # 有需要计算的关节
+                p0x = pts[keyPoint[:, 0], 0].asnumpy()
+                p0y = pts[keyPoint[:, 0], 1].asnumpy()
+                p1x = pts[keyPoint[:, 1], 0].asnumpy()
+                p1y = pts[keyPoint[:, 1], 1].asnumpy()
+                p2x = pts[keyPoint[:, 2], 0].asnumpy()
+                p2y = pts[keyPoint[:, 2], 1].asnumpy()
 
-                    # 角度
-                    angle1 = np.arctan2(p1y - p0y, p1x - p0x)
-                    angle2 = np.arctan2(p2y - p0y, p2x - p0x)
-                    angles[i][j] = np.abs(angle2 - angle1)
-                else:
-                    angles[i][j] = np.nan
+                angle1 = np.arctan2(p1y - p0y, p1x - p0x)
+                angle2 = np.arctan2(p2y - p0y, p2x - p0x)
+
+                angles[i][joint_computable] = np.abs(angle2 - angle1)
     
         return angles
 
@@ -56,7 +52,7 @@ class AngeleCal():
         scores = np.empty(len(angles))
         visibles = ~np.isnan(stdAngle)          # 样本中没有缺失值的
         stdAngle = stdAngle[visibles]           # 过滤掉样本中的缺失值
-        for i, angle in enumerate(angles):
+        for i, angle in enumerate(angles):      # 循环计算每个人的分数
             angle_v = angle[visibles]           # 过滤样本中也有缺失值的点
             if np.isnan(angle_v).any():         # 还有缺失值
                 scores[i] = np.nan
